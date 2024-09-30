696e195f7e162483dd22a2561e01041500a18a50
anaskaysar <kaysarul.apurba@northsouth.edu>
2024-09-30 19:58:24 -0400

    first commit

---
+++ Python Practices_Fall2024/For_loop.py
@@ -0,0 +1,7 @@
+expenses = [1000, 2000, 3000, 4000, 6000]
+
+total_expense = 0
+
+for expense in expenses:
+    total_expense = total_expense+expense
+print(total_expense)
---
+++ Python Practices_Fall2024/if_else.py
@@ -0,0 +1,8 @@
+n = input("Enter a number: ")
+
+n = int(n)
+
+if n % 2 == 0  :
+    print(f" {n} is even number")
+else : 
+    print(f"{n} is an odd number")

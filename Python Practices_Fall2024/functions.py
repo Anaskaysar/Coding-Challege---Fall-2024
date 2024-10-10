a56a3be5b2cd268cc81a565615410041f653083b
anaskaysar <kaysarul.apurba@northsouth.edu>
2024-10-10 11:14:27 -0400

    up

---
+++ Python Practices_Fall2024/functions.py
@@ -0,0 +1,13 @@
+expenses_kb = [30, 45, 70,80]
+expenses_ka = [11,16,20,55,24]
+
+total_expense = 0
+
+for expense in expenses_kb:
+    total_expense = total_expense+expense
+print(total_expense)
+
+total_expense2 = 0
+for expense in expenses_ka:
+    total_expense2 = total_expense2+expense
+print(total_expense2)
